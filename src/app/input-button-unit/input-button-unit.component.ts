d226bc9524f3ad7f651e1dbeccdd7f06996d9dc5
Sarah <mailerdresse@gmail.com>
2018-10-20 16:04:43 +0200

    add new component

---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  template: `
+    <p>
+      input-button-unit works!
+    </p>
+`,
+  styleUrls: ['./input-button-unit.component.css']
+})
+export class InputButtonUnitComponent implements OnInit {
+  constructor() { 
+  }
+  ngOnInit() {
+  }
+}
